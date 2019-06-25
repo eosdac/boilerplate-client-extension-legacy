ac85abaec0d925fcbb248e8a46d85ded1245cca3
unknown <kasperkasperfish@gmail.com>
2019-06-25 14:55:50 +0200

    remove redundant stuff

--- pages/home.vue
+++ pages/home.vue
@@ -13,8 +13,14 @@
         <div class="row gutter-sm">
           <div class="col-xs-12 col-lg-9 ">
             <div class="">
-              This is an example home page for your DAC
-              (src/extensions/pages/home.vue).
+              <div class="q-mb-md">
+                <span class="q-title">Example Homepage</span>
+                <span class="q-caption"> (src/extensions/pages/home.vue).</span>
+              </div>
+              <div class="text-text2">
+                <p>{{ $t("home.description_p1") }}</p>
+                <p>{{ $t("home.description_p2") }}</p>
+              </div>
             </div>
           </div>
           <div class="col-xs-12 col-lg-3 ">
@@ -23,14 +29,14 @@
                 v-if="!getAccountName"
                 size="md"
                 color="primary"
-                :label="$t('index.signin')"
+                :label="$t('home.signin')"
                 @click="login"
               />
               <q-btn
                 v-else
                 size="md"
                 color="primary"
-                :label="$t('index.signout')"
+                :label="$t('home.signout')"
                 @click="logout"
               />
             </div>
@@ -69,39 +75,6 @@ export default {
     },
     logout() {
       this.$store.dispatch("global/logout");
-    },
-
-    async subscribeNewsletter() {
-      this.onsubscribemsg = "";
-      let data = { email: this.email_address, language: this.language };
-
-      if (!this.$helper.isEmail(this.email_address) || this.language == "") {
-        this.onsubscribemsg = this.$t("index.valid_input_required");
-        return false;
-      }
-
-      // let url =
-      //   this.$helper.noBackSlash(this.$configFile.get("memberclientapi")) +
-      //   "/subscribe";
-
-      let url = "https://toolkit-api.eosdac.io/subscribe";
-
-      this.loading = true;
-      try {
-        let result = await this.$axios.post(url, data);
-        this.onsubscribemsg = this.$t("index." + result.data.message);
-        // console.log(result);
-      } catch (e) {
-        this.onsubscribemsg = this.$t("index.error_occured");
-        console.log(e);
-      }
-      this.clearForm();
-    },
-
-    clearForm() {
-      this.loading = false;
-      this.email_address = "";
-      this.language = "";
     }
   }
 };
@@ -109,32 +82,4 @@ export default {
 
 <style lang="stylus">
 @import '~variables'
-
-
-
-.index_border_right_col{
-  border-right:1px solid grey;
-}
-.row_boxes_index_page{
-  top:-100px;
-  transition: all .2s ease-in-out;
-}
-.partnerbox{
-  top:0px;
-  position:relative;
-}
-
-
-@media screen and (max-width: 1200px) {
-  .partnerbox{
-    top:40px!important;;
-  }
-  .row_boxes_index_page{
-    top:20px !important;
-  }
-  .index_border_right_col{
-    border-right: none !important;
-  }
-
-}
 </style>
